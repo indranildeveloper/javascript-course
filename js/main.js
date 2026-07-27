71060919f4a6a15531bb4747f06102ff1eb29f71
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-27 12:38:41 +0530

    numbers
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+// Numbers
+// JavaScript Numbers are Always 64-bit Floating Point
+
+// console.log(10);
+// console.log(20.6);
+
+// console.log(5 + 6);
+// console.log(5 - 6);
+// console.log(5 * 5);
+// console.log(6 / 3);
+// console.log(5 ** 5); // 5*5*5*5*5
+
+// console.log(58 % 3);
+
+// console.log(99999999999999);
+// console.log(999999999999999);
+// console.log(9999999999999999);
+
+// console.log(0.2 + 0.1);
+// console.log((0.2 * 10 + 0.1 * 10) / 10);
