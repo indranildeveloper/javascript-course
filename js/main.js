eef3181e7f83b905056bec61f5074373c1bd6566
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-29 13:05:25 +0530

    math object
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-// JavaScript null and undefined
+// JavaScript Math Object
 
-// In JavaScript, null is a primitive value that represents the intentional absence of any object value
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.LN2);
+// console.log(Math.LN10);
 
-// In JavaScript, undefined is a primitive data type and a global property that automatically signifies the absence of an assigned value
+// console.log(Math.round(5.6));
+// console.log(Math.round(5.2));
 
-// let user = null;
-// user = "John";
+// console.log(Math.ceil(1.2));
+// console.log(Math.floor(5.9));
 
-// let message = "hello";
-// console.log(message[100]);
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(100));
+// console.log(Math.abs(-6.5));
 
-let user;
+// console.log(Math.min(0, 5, -6, 7));
+// console.log(Math.max(0, 5, -6, 7));
 
-console.log(user);
+// console.log(Math.log(1));
+
+// console.log(Math.random());
+
+// return a random number from 1 to 10
+console.log(Math.floor(Math.random() * 10) + 1);
