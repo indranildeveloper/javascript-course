8ca52a92a967773908c4a56e4ed90ea8835a3ab8
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-29 16:16:23 +0530

    typeof operator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
-// JavaScript Math Object
+// JavaScript typeof operator
 
-// console.log(Math);
-// console.log(Math.PI);
-// console.log(Math.E);
-// console.log(Math.LN2);
-// console.log(Math.LN10);
+// The typeof operator returns the data type of a JavaScript variable.
 
-// console.log(Math.round(5.6));
-// console.log(Math.round(5.2));
+console.log(typeof "John");
+console.log(typeof ("John" + "Doe"));
+console.log(typeof 3.14);
+console.log(typeof 10);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 1234n);
+console.log(Symbol());
+console.log(typeof Symbol());
+console.log(typeof x);
+console.log(typeof undefined);
+console.log(typeof null);
 
-// console.log(Math.ceil(1.2));
-// console.log(Math.floor(5.9));
-
-// console.log(Math.pow(2, 3));
-// console.log(Math.sqrt(100));
-// console.log(Math.abs(-6.5));
-
-// console.log(Math.min(0, 5, -6, 7));
-// console.log(Math.max(0, 5, -6, 7));
-
-// console.log(Math.log(1));
-
-// console.log(Math.random());
-
-// return a random number from 1 to 10
-console.log(Math.floor(Math.random() * 10) + 1);
+console.log(typeof [1, 2, 3]);
