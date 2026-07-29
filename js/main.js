1571df63ce2bd59a0972e2fb5b36122b7b0e8746
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-29 18:52:49 +0530

    else statement
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 // JavaScript Conditionals
 
-let time = 8;
+let time = 100;
 let greeting = "Hi";
 
 if (time < 10) {
   console.log("Good Morning.");
   greeting = "Hello, Good Morning :)";
-} else if (time < 20) {
+} else if (time < 15) {
   console.log("Good Day.");
   greeting = "Hey, have a good day :)";
+} else if (time < 20) {
+  console.log("Good Afternoon.");
+  greeting = "Hey, have a good afternoon :)";
+} else if (time < 24) {
+  console.log("Good Night.");
+  greeting = "Hey, have a good night :)";
+} else {
+  console.log("Invalid time.");
+  greeting = "Hey set the time from 0 to 24.";
 }
 
 console.log(greeting);
