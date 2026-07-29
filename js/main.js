c2a2642233cd613bed9b16788983587a168eb815
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-29 12:36:19 +0530

    string template literals
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // JavaScript Strings
 
-let message = 'this is a backslash -> \\ he said, \n "hi there"';
+let firstName = "John";
+let lastName = "Doe";
+
+// let message = "Your full name is " + firstName + " " + lastName + "!";
+
+let message = `Your full name is ${firstName.toUpperCase()} ${lastName.toUpperCase()}!`;
+
 console.log(message);
+
+let price = 10;
+let tax = 0.25;
+
+let total = `Your total is: $${(price * (1 + tax)).toFixed(2)}`;
+
+console.log(total);
