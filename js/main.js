8d13954e9276f1f16184893fe6bb0ca2db07811e
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-29 11:54:28 +0530

    string escape characters
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 // JavaScript Strings
 
-let message = "Hello world, welcome to the universe. red, red, red, blue";
-// console.log(message.indexOf("world", 10));
-// console.log(message.indexOf("red"));
-// console.log(message.indexOf("Red"));
-// console.log(message.slice(0, 5));
-// console.log(message.slice(-2));
-
-// let result = message.replaceAll("red", "blue");
-
-// console.log(result);
+let message = 'this is a backslash -> \\ he said, \n "hi there"';
+console.log(message);
