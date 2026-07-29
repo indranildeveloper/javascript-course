7d943d09beca6c9c1ba914fc38839a1c0eb72d31
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-29 17:08:55 +0530

    equality comparison operators
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 // JavaScript Comparison Operators
 
-// console.log(5 > 6);
-// console.log(10 > 8);
-// console.log(6 < 7);
-// console.log(8 < 5);
-
 let x = 10;
 
-// console.log(x <= 20);
-// console.log(x <= 10);
-// console.log(x <= 6);
+// console.log(x == 10);
+// console.log(x == "10");
+// console.log(x == 20);
+// console.log(x == true);
+// console.log(0 == false);
+// console.log(0 == "");
+// console.log(null == undefined);
 
-// console.log(x >= 20);
-// console.log(x >= 10);
-// console.log(x >= 5);
+// console.log(x === 10);
+// console.log(x === "10");
+// console.log(0 === false);
+// console.log(0 === "");
+// console.log(null === undefined);
 
-// console.log("a" > "A");
-// console.log("a" > "B");
+// console.log(x != 10);
+// console.log(x != "10");
 
-// console.log("$" > "#");
-
-// console.log("a".charCodeAt(0));
+// console.log(x !== 10);
+// console.log(x !== "10");
