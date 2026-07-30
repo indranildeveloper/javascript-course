7fce3302520e7a22bbee3b43d035c83493d7d029
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-30 15:55:35 +0530

    switch statement
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
 // @ProgrammingWithIndra
 
-let x = 5;
+let day = 6;
 
-let result = x == 5 || (x === 3 && x > 10);
+switch (day) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Hey please select a day between 1 and 7.");
+}
 
-console.log(result);
+switch (day) {
+  case 1:
+  case 6:
+  case 7:
+    console.log("I love weekends.");
+    break;
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log("Working hard.");
+    break;
+  default:
+    console.log("Please select a valid date.");
+}
