b354889a684fbeb99ccfc55781516f4fd2a6cee2
Indranil Halder <indranilhalder.dev@gmail.com>
2026-07-30 12:40:21 +0530

    logical and operator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 // @ProgrammingWithIndra
 
-// Falsy Values
-// false
-// 0, -0, 0n
-// "", '', ``
-// null
-// undefined
-// NaN
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
 
-let user = "john";
+// let password = "abc123";
 
-if (user) {
-  console.log("We have a user.");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Valid password.");
+// } else {
+//   console.log("Invalid password.");
+// }
+
+let number = 8;
+
+if (number >= 0 && number <= 10) {
+  console.log("Valid number.");
 } else {
-  console.log("We do not have a user.");
+  console.log("Please guess a number from 0 to 10.");
 }
