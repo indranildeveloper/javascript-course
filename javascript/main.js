5a55f0a796f195af4fd793674798f4ec7e09e60f
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-27 17:38:16 +0530

    NaN, Infinity and Zero
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,10 @@
 //  @ProgrammingWithIndra
-console.log(10);
-console.log(3.14);
-console.log(typeof 10);
-console.log(typeof 3.14);
-console.log(123e5);
-console.log(123e-5);
-console.log(9999999999999999);
-console.log(10 + 10);
-console.log(10 - 5);
-console.log(10 * 5);
-console.log(10 / 5);
-console.log(14 % 5);
-console.log(5 ** 3);
-console.log(10 * (5 - 6));
-console.log(0.1 + 0.2);
-console.log((0.1 * 10 + 0.2 * 10) / 10);
+console.log(0 / 0);
+console.log(NaN);
+console.log(NaN + 5);
+console.log(typeof NaN);
+console.log(5 / 0);
+console.log(Infinity);
+console.log(-5 / 0);
+console.log(0);
+console.log(-0);
