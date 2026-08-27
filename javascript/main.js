453d853462704d162a4c7dccaa8eb40962c56ac4
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-27 17:31:27 +0530

    numbers
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,17 @@
-/* 
-this
-is 
-a 
-multiline 
-comment
-*/
-
-// This will print Hello World to the console
-console.log("Hello World!");
-// console.log("Testing");
+//  @ProgrammingWithIndra
+console.log(10);
+console.log(3.14);
+console.log(typeof 10);
+console.log(typeof 3.14);
+console.log(123e5);
+console.log(123e-5);
+console.log(9999999999999999);
+console.log(10 + 10);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(14 % 5);
+console.log(5 ** 3);
+console.log(10 * (5 - 6));
+console.log(0.1 + 0.2);
+console.log((0.1 * 10 + 0.2 * 10) / 10);
