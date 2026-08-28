61277a54d2a3cce4a37f1c7cc895fe80332a5ecc
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-28 16:39:25 +0530

    update variables
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,14 @@
 //  @ProgrammingWithIndra
 
-// Variables
-// JavaScript variables are containers for data.
-
 let age = 10;
-let Age = 15;
-console.log(age + 10);
 
-age = age + 5;
+// age = age + 2;
+// age += 2;
+// age -= 5;
+// age *= 5;
+// age /= 2;
+
+// age++;
+// age--;
 
 console.log(age);
-// console.log(Age);
-
-// let myLongVariable = 5;
-// let x = 20;
-// let firstName = "John";
-// let lastName;
-// console.log(lastName);
-
-let _age = 10;
-let $age = 15;
