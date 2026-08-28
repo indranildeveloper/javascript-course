bb87c2b051b378b2b9e7618d65042a9dd11d49d0
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-28 16:50:43 +0530

    const keyword
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,15 @@
 //  @ProgrammingWithIndra
 
-let age = 10;
+// The const keyword was introduced in ES6 (2015)
+// Variables defined with const cannot be Redeclared
+// Variables defined with const cannot be Reassigned
+// Variables defined with const have Block Scope
 
+// let age = 10;
 // age = age + 2;
-// age += 2;
-// age -= 5;
-// age *= 5;
-// age /= 2;
+// console.log(age);
 
-// age++;
-// age--;
-
+const age = 10;
+// age = age + 2;
+// let age = 20;
 console.log(age);
