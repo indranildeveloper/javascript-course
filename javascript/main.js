68f43913d667d15b540ac7014ebe28629516a0fb
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-28 17:35:24 +0530

    string indices
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,12 @@
 //  @ProgrammingWithIndra
 
-// Strings are for storing text
-// Strings are written with quotes
+// Strings are indexed
 
-let firstName = "John";
-let lastName = "Doe";
+let message = "Hello World";
+console.log(message.length);
+console.log(message[0]);
+console.log(message[message.length - 1]);
 
-let message = "he said, 'lol'";
-// let num = 5;
+message[0] = "K";
 
-let fullName = firstName + " " + lastName;
-
-console.log(fullName);
-
-// console.log("hi" + 5);
-// console.log("hi" - "k");
-
-// console.log(firstName);
-// console.log(lastName);
-// console.log(message);
-// console.log(typeof num);
+console.log(message);
