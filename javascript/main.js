dadaf4e69ea80afae01954ccf2d29581f9e47a89
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-29 20:32:00 +0530

    typeof operator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,18 @@
 //  @ProgrammingWithIndra
 
-// Math Object
+let x;
 
-// console.log(Math.E);
-// console.log(Math.PI);
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.1));
-// console.log(Math.floor(5.6));
-// console.log(Math.pow(5, 3));
-// console.log(Math.sqrt(64));
-// console.log(Math.abs(-7));
-// console.log(Math.min(0, 1, -5, 6));
-// console.log(Math.max(0, 1, -5, 6));
+console.log(typeof "John");
+console.log(typeof ("John" + "Doe"));
+console.log(typeof 7);
+console.log(typeof 5.6);
+console.log(typeof (5 + 4));
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 1234n);
+console.log(typeof Symbol());
+console.log(x);
+console.log(typeof x);
+console.log(typeof null);
 
-// console.log(Math.random());
-
-// Random number from 1 to 10
-const randomNumber = Math.floor(Math.random() * 10) + 1;
-console.log(randomNumber);
+console.log(typeof [1, 2, 3]);
