f5a0a704322214632983bbdb30c5ace19e119c00
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-29 16:25:25 +0530

    template strings
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,24 @@
 //  @ProgrammingWithIndra
 
-// Escape Characters
+// Template Strings
 
-let message = "he said, 'lol'";
-let messageTwo = "this is on a\nnew line.";
-let messageThree = "this is separated by a\ttab";
+let firstName = "John";
+let lastName = "Doe";
+
+// Welcome: John Doe!
+// let greeting = "Welcome: " + firstName + " " + lastName + "!";
+
+let greeting = `Welcome: ${firstName.toUpperCase()} ${lastName}!`;
+
+console.log(greeting);
+
+let price = 10;
+let tax = 0.25;
+
+let total = `Total: $${(price * (1 + tax)).toFixed(2)}`;
+console.log(total);
+
+let message = `He 
+said,
+"lol"`;
 console.log(message);
-console.log(messageTwo);
-console.log(messageThree);
