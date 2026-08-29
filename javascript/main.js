6dbc3c53c3b129bb17094b55406482d32b6e29bb
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-29 15:35:31 +0530

    more string methods
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,18 @@
 //  @ProgrammingWithIndra
 
 // String Methods
-// toUpperCase
-// toLowerCase
-// trim
+// indexOf
+// slice
+// replace
+// replaceAll -> ES2021
 
-let message = "Hello World";
-let messageTwo = message.toUpperCase();
-let messageThree = message.toLowerCase();
-console.log(messageTwo);
-console.log(messageThree);
+let message = "Hello World. Hello World. Hello World.";
+console.log(message.indexOf("World"));
+console.log(message.indexOf("z"));
+console.log(message.slice(6, 11));
+console.log(message.slice(0));
 
-let text = "     hi there      ";
-console.log(text.trim());
-console.log(text.trimStart());
-console.log(text.trimEnd());
+console.log(message.replace("Hello", "Hi There"));
+console.log(message.replaceAll("Hello", "Hi There"));
+
+console.log(message);
