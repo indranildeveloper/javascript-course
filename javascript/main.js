ee1684aa5beb75e94ffefecd1d2f29da2339fb5e
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-30 16:44:03 +0530

    strict equality operator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,17 @@
 // @ProgrammingWithIndra
 
-// console.log(5 == 5);
-// console.log(5 == 7);
 // console.log(5 == "5");
+// console.log(5 === "5");
+// console.log(5 === 5);
+
 // console.log(1 == true);
-// console.log(0 == false);
+// console.log(1 === true);
+
+// console.log(0 == "");
+// console.log(0 === "");
+
 // console.log(null == undefined);
-console.log(5 != 5);
-console.log(5 != 7);
+// console.log(null === undefined);
+
 console.log(5 != "5");
+console.log(5 !== "5");
