724153ad1851a0a77f53ee98ff773b0de258ff0c
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-30 17:14:56 +0530

    else statement
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,9 @@
 // @ProgrammingWithIndra
 
-let time = 20;
-let greeting = "Hello";
+let num = 8;
 
-if (time < 5) {
-  greeting = "Good Night.";
-} else if (time < 12) {
-  greeting = "Good Morning.";
-} else if (time < 17) {
-  greeting = "Good Afternoon.";
-} else if (time < 22) {
-  greeting = "Good Evening.";
+if (num % 2 === 0) {
+  console.log("Event number.");
+} else {
+  console.log("Odd number.");
 }
-
-console.log(greeting);
