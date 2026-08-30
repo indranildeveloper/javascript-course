0034bf7a7a813383d596d5498b8b581cb2d43a43
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-30 16:27:05 +0530

    comparison operators
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,14 @@
 // @ProgrammingWithIndra
 
-// console.log(parseInt("10"));
-// console.log(parseInt("10.05"));
-// console.log(parseInt("10.99"));
-// console.log(parseInt("50 60 70"));
-// console.log(parseInt("  50  "));
-// console.log(parseInt("50 years"));
-// console.log(parseInt("He is 50"));
+let age = 20;
 
-console.log(parseFloat("10"));
-console.log(parseFloat("10.65"));
-console.log(parseFloat("10 20 30"));
-console.log(parseFloat("10years"));
-console.log(parseFloat("he is 10"));
+console.log(age > 18);
+console.log(age < 18);
+console.log(age >= 20);
+console.log(age >= 18);
+console.log(age <= 20);
+console.log(age <= 18);
+
+console.log("a" > "A");
+console.log(97 > 65);
+console.log("$" > "@");
