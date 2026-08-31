6d3f5302817960fa013a9b193e1f5f9e46aa6125
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-31 11:36:37 +0530

    logical and operator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,13 @@
 // @ProgrammingWithIndra
 
-// Truthy and Falsy Values
-// false: The boolean keyword itself.
-// 0: The number zero.
-// -0: Negative zero.
-// 0n: BigInt zero.
-// "" or '' or ``: Any empty string.
-// null: The absence of any value.
-// undefined: A variable that has been declared but not assigned a value.
-// NaN: "Not-a-Number".
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
 
-let mystery = 5;
+let x = 8;
+let y = 5;
 
-if (mystery) {
-  console.log("Truthy.");
-} else {
-  console.log("Falsy.");
+if (x > 5 && y > 2) {
+  console.log("It is good.");
 }
