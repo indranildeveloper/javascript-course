1705b02a978b7e61d50eaedc324059967e3861d2
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-31 12:30:48 +0530

    nullish coalescing operator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,6 @@
 // @ProgrammingWithIndra
 
-// console.log(!true);
-// console.log(!false);
-
-// let user = "john";
-
-// if (!user) {
-//   console.log("There is no user.");
-// } else {
-//   console.log("We have a user.");
-// }
-
-let x = 5 === 7;
-let y = !(5 === 7);
-
-if (!(x || y)) {
-  console.log("Valid condition.");
-} else {
-  console.log("Invalid condition.");
-}
-
-// console.log(x);
-// console.log(y);
-
-let user = null;
-
-console.log(!!user);
+let user = "john";
+let text = "no user.";
+let result = user ?? text;
+console.log(result);
