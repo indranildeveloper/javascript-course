db0f8d2c73ba17ed0b21e23c5bae24f785a5cc88
Indranil Halder <indranilhalder.dev@gmail.com>
2026-08-31 16:56:40 +0530

    array introduction
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,6 @@
 // @ProgrammingWithIndra
 
-let day = 6;
-
-switch (day) {
-  case 0:
-    console.log("Sunday");
-    break;
-  case 1:
-    console.log("Monday");
-    break;
-  case 2:
-    console.log("Tuesday");
-    break;
-  case 3:
-    console.log("Wednesday");
-    break;
-  case 4:
-    console.log("Thursday");
-    break;
-  case 5:
-    console.log("Friday");
-    break;
-  case 6:
-    console.log("Saturday");
-    break;
-  default:
-    console.log("Please add a valid date (0 - 6).");
-}
-
-switch (day) {
-  case 4:
-  case 5:
-    console.log("Soon it will be weekend.");
-    break;
-  case 0:
-  case 6:
-    console.log("It is weekend.");
-    break;
-  default:
-    console.log("Working hard.");
-}
+// let fruits = ["banana", "cherry", "apple"];
+let fruits = new Array("apple", "banana", "cherry", "orange");
+console.log(fruits);
+console.log(fruits.length);
