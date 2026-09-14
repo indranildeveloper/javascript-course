f262411dc8dc45dc3da09e9f1c8b92409d5da268
Indranil Halder <indranilhalder.dev@gmail.com>
2026-09-14 21:15:12 +0530

    break statements
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,5 @@
 // @ProgrammingWithIndra
 
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i);
-// }
-
-// let j = 1;
-// while (j <= 10) {
-//   console.log(j);
-//   j++;
-// }
-
-// let k = 15;
-
-// do {
-//   console.log(k);
-//   k++;
-// } while (k <= 10);
-
 const target = Math.floor(Math.random() * 10);
 let guess = Math.floor(Math.random() * 10);
 
@@ -26,3 +9,10 @@ while (target !== guess) {
 }
 
 console.log("Hey you win the game.");
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
