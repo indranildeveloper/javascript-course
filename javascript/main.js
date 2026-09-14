980cc78e0e01003d1e6454aa3fb03ebae82f8772
Indranil Halder <indranilhalder.dev@gmail.com>
2026-09-14 12:24:04 +0530

    infinite loops
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,7 @@
 // @ProgrammingWithIndra
 
-// for (let i = 1; i <= 25; i += 5) {
-//   console.log("Hello: ", i);
-// }
-
-// for (let i = 51; i <= 60; i++) {
-//   console.log("hello", i);
-// }
-
-for (let num = 50; num > 0; num -= 10) {
-  console.log("Hello", num);
+for (let i = 10; i > 0; i--) {
+  console.log(i);
 }
 
 console.log("after the loop");
